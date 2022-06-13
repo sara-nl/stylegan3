da08d9c79dcbe6a10e569b158b4874c683d1dfc3
Caspar van Leeuwen <casparl@gcn21.local.snellius.surf.nl>
2022-06-13 10:05:51 +0200

    Corrected the source

--- load_env.sh
+++ load_env.sh
@@ -3,4 +3,4 @@ module load PyTorch/1.11.0-foss-2021a-CUDA-11.6.0
 module load matplotlib/3.4.2-foss-2021a
 module load tqdm/4.61.2-GCCcore-10.3.0
 
-source ~/.virtualenvs/NKI_STYLEGAN3
+source ~/.virtualenvs/NKI_STYLEGAN3/bin/activate
